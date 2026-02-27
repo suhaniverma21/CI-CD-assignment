f7e0fb66c9aff5ab8f994d665887261eb76eeb71
suhaniverma21 <suhani.verma@kickdrumtech.com>
2026-02-27 11:37:21 +0530

    Initial Spring Boot app with correct structure

---
+++ src/main/java/com/example/mock_cleanly/MockCleanlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mock_cleanly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCleanlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockCleanlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mock_cleanly/MockCleanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mock_cleanly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockCleanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
